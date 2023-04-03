21311907b297cffc1bd2b366494b624803aec87c
Daniel Bourdrez <3641580+bytemare@users.noreply.github.com>
2023-04-03 13:13:37 +0200

    Revert "Use bigmod instead of math/big"

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module github.com/bytemare/hash2curve
 
 go 1.20
 
-require (
-	filippo.io/bigmod v0.0.1
-	github.com/bytemare/hash v0.1.5
-)
+require github.com/bytemare/hash v0.1.5
 
 require (
 	golang.org/x/crypto v0.7.0 // indirect
--- h2f.go
+++ h2f.go
@@ -11,19 +11,14 @@ package hash2curve
 
 import (
 	"crypto"
+	"math/big"
 
-	"filippo.io/bigmod"
 	"github.com/bytemare/hash"
 )
 
 // HashToFieldXOF hashes the input with the domain separation tag (dst) to an integer under modulo, using an
 // extensible output function (e.g. SHAKE).
-func HashToFieldXOF(
-	id hash.Extendable,
-	input, dst []byte,
-	count, ext, securityLength int,
-	modulo *bigmod.Modulus,
-) []*bigmod.Nat {
+func HashToFieldXOF(id hash.Extendable, input, dst []byte, count, ext, securityLength int, modulo *big.Int) []*big.Int {
 	expLength := count * ext * securityLength // elements * ext * security length
 	uniform := ExpandXOF(id, input, dst, expLength)
 
@@ -32,20 +27,15 @@ func HashToFieldXOF(
 
 // HashToFieldXMD hashes the input with the domain separation tag (dst) to an integer under modulo, using an
 // merkle-damgard based expander (e.g. SHA256).
-func HashToFieldXMD(
-	id crypto.Hash,
-	input, dst []byte,
-	count, ext, securityLength int,
-	modulo *bigmod.Modulus,
-) []*bigmod.Nat {
+func HashToFieldXMD(id crypto.Hash, input, dst []byte, count, ext, securityLength int, modulo *big.Int) []*big.Int {
 	expLength := count * ext * securityLength // elements * ext * security length
 	uniform := ExpandXMD(id, input, dst, expLength)
 
 	return reduceUniform(uniform, count, securityLength, modulo)
 }
 
-func reduceUniform(uniform []byte, count, securityLength int, modulo *bigmod.Modulus) []*bigmod.Nat {
-	res := make([]*bigmod.Nat, count)
+func reduceUniform(uniform []byte, count, securityLength int, modulo *big.Int) []*big.Int {
+	res := make([]*big.Int, count)
 
 	for i := 0; i < count; i++ {
 		offset := i * securityLength
@@ -55,14 +45,12 @@ func reduceUniform(uniform []byte, count, securityLength int, modulo *bigmod.Mod
 	return res
 }
 
-func reduce(input []byte, modulo *bigmod.Modulus) *bigmod.Nat {
+func reduce(input []byte, modulo *big.Int) *big.Int {
 	/*
 		Interpret the input as a big-endian encoded unsigned integer of the field, and reduce it modulo the prime.
 	*/
-	i, err := bigmod.NewNat().SetBytes(input, modulo)
-	if err != nil {
-		panic(err)
-	}
+	i := new(big.Int).SetBytes(input)
+	i.Mod(i, modulo)
 
 	return i
 }
